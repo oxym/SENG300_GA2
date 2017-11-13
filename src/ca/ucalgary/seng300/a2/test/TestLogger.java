e23cc439cf5c74f4196e553a864241f3aff6659f
Khesualdo Condori <medvegalapa@gmail.com>
2017-11-12 21:44:26 -0700

    Added comments to TestLogger.java

--- src/ca/ucalgary/seng300/a2/test/TestLogger.java
+++ src/ca/ucalgary/seng300/a2/test/TestLogger.java
@@ -8,34 +8,67 @@ import java.io.FileNotFoundException;
 import ca.ucalgary.seng300.a2.Logger;
 
 public class TestLogger {
-
+	
+	/**
+	 * Test Logger constructor with null for filename
+	 * @throws IllegalArgumentException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testConstructorNull() throws IllegalArgumentException, NullPointerException {
 		new Logger(null);
 	}
 	
+	/**
+	 * Test Logger constructor with empty filename
+	 * @throws IllegalArgumentException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructorEmpty() throws IllegalArgumentException, NullPointerException {
 		new Logger("");
 	}
 	
+	/**
+	 * Test Logger constructor with valid filename
+	 * @throws IllegalArgumentException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test
 	public void testConstructorName() throws IllegalArgumentException, NullPointerException {
 		new Logger("temp.txt");
 	}
 	
+	/**
+	 * Test log(String msg) method with valid message
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test
 	public void testLogMsg() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
 		logger.log("Hello World!");
 	}
 	
+	/**
+	 * Test log(String msg) method with an empty message
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testLogEmpty() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
 		logger.log("");
 	}
 
+	/**
+	 * Test log(String msg) method with a null message
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testLogNull() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
@@ -43,6 +76,12 @@ public class TestLogger {
 		logger.log(msg);
 	}
 	
+	/**
+	 * Test log(String[] msgs) method with a valid array of messages
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test
 	public void testArrayLogMsg() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
@@ -50,6 +89,12 @@ public class TestLogger {
 		logger.log(msgs);
 	}
 	
+	/**
+	 * Test log(String[] msgs) method with an array of messages where one message is empty
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testArrayLogEmpty() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
@@ -57,6 +102,12 @@ public class TestLogger {
 		logger.log(msgs);
 	}
 	
+	/**
+	 * Test log(String[] msgs) method with a valid array of messages where one message is null
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testArrayLogNull() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
@@ -64,6 +115,13 @@ public class TestLogger {
 		logger.log(msgs);
 	}
 	
+	/**
+	 * Test log(String[] msgs) method with a valid array of messages 
+	 * where first invalid message is the empty message
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testArrayLogEmptyNull() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
@@ -71,6 +129,13 @@ public class TestLogger {
 		logger.log(msgs);
 	}
 	
+	/**
+	 * Test log(String[] msgs) method with a valid array of messages 
+	 * where first invalid message is null
+	 * @throws IllegalArgumentException if error
+	 * @throws FileNotFoundException if error
+	 * @throws NullPointerException if error
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testArrayLogNullEmpty() throws IllegalArgumentException, FileNotFoundException, NullPointerException {
 		Logger logger = new Logger("temp.txt");
