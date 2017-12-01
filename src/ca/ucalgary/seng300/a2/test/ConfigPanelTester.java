3953569554280acece60bf3e865046d163cd2fa4
Michael <xiangyu.han@ucalgry.ca>
2017-12-01 12:37:18 -0600

    Add a tester for Configuration panel

---
+++ src/ca/ucalgary/seng300/a2/test/ConfigPanelTester.java
@@ -0,0 +1,14 @@
+package ca.ucalgary.seng300.a2.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfigPanelTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
