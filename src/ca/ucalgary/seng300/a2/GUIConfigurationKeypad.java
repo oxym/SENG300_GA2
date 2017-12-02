cf76b0a51da1a7374a9b262e01a83029fd139e10
T Co <thomas.coderre@ucalgary.ca>
2017-12-02 16:38:01 -0700

    Updated keypad layout

--- src/ca/ucalgary/seng300/a2/GUIConfigurationKeypad.java
+++ src/ca/ucalgary/seng300/a2/GUIConfigurationKeypad.java
@@ -16,19 +16,17 @@ public class GUIConfigurationKeypad extends GUIPanel {
 	private static final long serialVersionUID = 1803496343446827611L;
 	private JButton[] jButtons;
 //	private JButton[] numberButtons;
-	private String[] buttonLabels = { "q", "w", "e", "r", "t", "y", "u", "i",
-										"o", "p", "a", "s",
-										"d", "f", "g", "h", "j", "k", "l", "z",
-										"x", "c", "v", "b" ,"n", "m",
-										"Shift", "Enter", "0", "1","2", "3", "4", 
-										"5", "6", "7", "8", "9"};
+	private String[] buttonLabels = { "0", "1","2", "3", "4","5", "6", "7", "8", "9",
+									  "q", "w", "e", "r", "t", "y", "u", "i","o", "p", 
+									  "a", "s","d", "f", "g", "h", "j", "k", "l","Enter", 
+									  "Shift","z","x", "c", "v", "b" ,"n", "m"};
 
 
 	@Override
 	void init() {
 		//setLayout(grid);
 		jButtons = new JButton[buttonLabels.length];
-
+		
 		for(int i = 0; i < buttonLabels.length; i++) {
 			jButtons[i] = new JButton(buttonLabels[i]);
 			add(jButtons[i]);
