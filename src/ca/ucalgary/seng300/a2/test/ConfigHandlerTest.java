cbc6e5cd4fdc9fc3790a5ea4a58bcc205a196c44
Michaela <mikimika@gmail.com>
2017-12-01 17:32:46 -0700

    First test case started for getButtonDisplayCode

--- src/ca/ucalgary/seng300/a2/test/ConfigHandlerTest.java
+++ src/ca/ucalgary/seng300/a2/test/ConfigHandlerTest.java
@@ -1,10 +1,54 @@
 package ca.ucalgary.seng300.a2.test;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
+import org.lsmr.vending.hardware.VendingMachine;
+
+import ca.ucalgary.seng300.a2.ConfigPanelHandler;
+import ca.ucalgary.seng300.a2.DispListener;
+import ca.ucalgary.seng300.a2.MachineConfiguration;
+import ca.ucalgary.seng300.a2.VendingManager;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class ConfigHandlerTest {
+	private VendingMachine machine = null;
+	private VendingManager manager = null;
+	private MachineConfiguration cfg;
+	private ConfigPanelHandler configH;
+
 	@Before
-	public void setup(){
-		
+	public void setup() throws Exception {
+
+		cfg = new MachineConfiguration();
+
+		machine = new VendingMachine(cfg.coinKinds, cfg.selectionButtonCount, cfg.coinRackCapacity, cfg.productRackCapacity,
+				cfg.receptacleCapacity, cfg.deliveryChuteCapacity, cfg.coinReturnCapacity);
+		machine.configure(cfg.productNames, cfg.productCosts);
+
+		manager = VendingManager.initialize(machine, cfg.coinKinds);
+
+		machine.disableSafety(); //needed due to singleton instance being passed to multiple tests
+
+		configH = new ConfigPanelHandler(manager);
 	}
+	
+	/*
+	 * Tests for getButtonDisplayCode
+	 */
+	@Test
+	public void invalidButton(){	
+		String code = configH.getButtonDisplayCode(-1);
+		System.out.println(code);
+		assertEquals("", configH.getButtonDisplayCode(1));
+	}
+	
+	@Test
+	public void invalidButton2(){	
+		String code = configH.getButtonDisplayCode(1);
+		System.out.println(code);
+		assertEquals("B", configH.getButtonDisplayCode(1));
+	}
+	
 }
