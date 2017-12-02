26d6ac192109394c333afdfdfa94ac6622cbfbae
Khesualdo Condori <medvegalapa@gmail.com>
2017-12-02 12:40:24 -0700

    Added generality to SelectionBtns

--- src/ca/ucalgary/seng300/a2/GUISelectionButtons.java
+++ src/ca/ucalgary/seng300/a2/GUISelectionButtons.java
@@ -2,6 +2,8 @@ package ca.ucalgary.seng300.a2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUISelectionButtons extends GUIPanel {
 
@@ -11,23 +13,19 @@ public class GUISelectionButtons extends GUIPanel {
 	private JPanel buttonPanel;
 
 	private JLabel title;
-
-	// TODO: remove
-	private JButton pop1;
-	private JButton pop2;
-	private JButton pop3;
-	private JButton pop4;
+	
+	private JButton[] btns;
+	
+	private SelectionButtonListener listener;
 
 	@Override
 	void init() {
 		
+		listener = new SelectionButtonListener();
+		
 		setBackground(COLOR_BACKGROUND);
-		title = new JLabel("Selection Buttons Here");
+		title = new JLabel("Make your choice!");
 		setOpaque(false);
-
-		// TODO: add actionListeners for the selection buttons
-
-		// New code
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -41,33 +39,59 @@ public class GUISelectionButtons extends GUIPanel {
 		gbc.gridy = 0;
 		titlePanel.add(title);
 		gbc.gridx = 0;
-		gbc.gridy = 1;
+		gbc.gridy = 0;
 		add(titlePanel, gbc);
 
 		// Create buttonPanel
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridBagLayout());
 		buttonPanel.setBackground(COLOR_BACKGROUND);
+		
+		// Number of different types of pops in Vending Machine
+		int popTypes = GUIMain.getVendingManager().getNumberOfSelectionButtons();
+		btns = new JButton[popTypes];
+		
+		// Populate btns array with JButtons
+		for (int i = 0; i < btns.length; ++i) {
+			btns[i] = new JButton(GUIMain.getVendingManager().getProductName(i));
+			btns[i].addActionListener(listener);
+		}
 
-		// TODO: replace with pops from VM
-		pop1 = new JButton("POP 1");
-		pop2 = new JButton("POP 2");
-		pop3 = new JButton("POP 3");
-		pop4 = new JButton("POP 4");
-
-		JButton[] buttonArray = new JButton[]{pop1, pop2, pop3, pop4};
-		for(int i=0; i < 3; i++){
-		    gbc.gridx = i;
-		    gbc.gridy = 0;
-		    buttonPanel.add(buttonArray[i], gbc);
+		int y = 0;
+		int x = 0;
+		int counter = 0;
+		
+		// Place JButtons on buttonPanel
+		for(int i=0; i < btns.length; i++){
+			
+		    gbc.gridx = x;
+		    gbc.gridy = y;
+		    buttonPanel.add(btns[i], gbc);
+		    
+		    if (counter >= popTypes / 3) {
+		    		y += 1;
+		    		x = 0;
+		    		counter = 0;
+		    } else {
+			    counter += 1;
+			    x += 1;
+		    }
 		}
 
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		add(buttonPanel, gbc);
 
-		// New code
-
 		setVisible(true);
+	}
+	
+	private class SelectionButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			String btnName = event.getActionCommand();
+			
+			// TODO: connect btn click to VM
+		}
 	}
 }
